ddb216299c3900c0160962e7d310ec00a6758ed7
Mohamad Sheikhahmadi <s327914@studenti.polito.it>
2025-05-18 13:17:09 +0000

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,6 +81,6 @@ if st.button("Run Forecast"):
         })
 
         # Streamlit chart
-        st.line_chart(df_combined,use_container_width=False)
+        st.line_chart(df_combined,height=100)
     except Exception as e:
         st.error(f"❌ Forecasting failed: {e}")
