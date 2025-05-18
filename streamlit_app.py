7efe673ebc0179669663d7053921745e225b02d9
Mohamad Sheikhahmadi <s327914@studenti.polito.it>
2025-05-18 13:05:51 +0000

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ st.set_page_config(layout="wide")
 
 st.title("📈 Bitcoin Price Forecast using Holt-Winters")
 
-start_date = st.date_input("Start Date", pd.to_datetime("2014-01-01"))
+start_date = st.date_input("Start Date", pd.to_datetime("2024-01-01"))
 end_date = st.date_input("End Date")
 
 if start_date >= end_date:
